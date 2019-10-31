8dac22222982c4ca34fa9a8ce1353cdc5a7fae89
Jacob <wittenberg.jacob@gmail.com>
2019-10-30 20:25:13 -0500

    authentication function works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,13 @@
-// import firebase from 'firebase';
+import firebase from 'firebase';
 
 import auth from './components/Auth/auth';
+
+import apiKeys from './helpers/data/apiKeys.json';
 
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.loginButton();
 };
 
